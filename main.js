d131e0d0e47f003af2b201eedc1478658dfb333e
Joseph Rosario <josephrosario09@gmail.com>
2022-04-29 17:44:07 +1000

    reset button and dynamic grid added. need to add RGB option and design to look like etchasketch

--- main.js
+++ main.js
@@ -1,18 +1,67 @@
-const grid = document.querySelector(".grid");
+const reset = document.querySelector('.reset');
 
-function repeatDiv() {
-    for (let i = 0; i < 3; i++) {
-        let div = document.createElement("div");
-        div.classList.add("boxes");
-        grid.appendChild(div);
+function createBoard(size) {
+
+    let grid = document.querySelector(".grid");
+    grid.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+    let square = document.querySelectorAll('.square');
+        square.forEach((div) => div.remove());
+    for (let i = 0; i < size * size; i++) {
+        let square = document.createElement('div');
+        square.style.backgroundColor = 'white';
+        grid.appendChild(square);
+        square.className = 'square';
+        square.addEventListener('mouseover', function () {
+            this.style.backgroundColor = 'black';
+        });
+
     }
 }
 
-//function createDiv() {
-//    const newDiv = document.createElement("div");
-//    newDiv.classList.add("boxes");
-//    grid.appendChild(newDiv);
+createBoard(16);
+
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+        createBoard(input);
+    } else {
+        console.log("too many squares");
+    }
+    return;
+}
+
+reset.addEventListener("click", function(){
+    let square = document.querySelectorAll('.square');
+    square.forEach((div) => div.style.backgroundColor = 'white');
+})
+
+//let setSize = document.querySelector(".set-size")
+//setSize.addEventListener("click",function(){
+//    prompt('How many rows? Max 100')
+//
+//});
+
+//function createDiv(v) {
+//
+//    for (let i = 0; i < v; i++) {
+//        let row = document.createElement("div");
+//        row.className = "row";
+//        for (let x = 1; x <= v; x++) {
+//            let cell = document.createElement("div");
+//            cell.className = "gridsquare";
+//            cell.innerText = (i * v) + x;
+//            row.appendChild(cell);
+//            row.addEventListener('mouseover', function () {
+//                this.style.backgroundColor = 'blue';
+//            }); 
+//            cell.addEventListener('mouseover', function () {
+//                this.style.backgroundColor = 'blue';
+//            })
+//        }
+//        grid.appendChild(row);
+//    }
+//    document.getElementsByClassName("grid").innerText = grid.innerHTML;
 //}
-
-
-repeatDiv();
+//
+//createDiv(16);
+// 
